f8129a31a7198ddf7dc8b19f4f93a92628efbc56
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2022-11-26 12:04:25 +0900

    コントラクトコードのドラフト完了

--- astar_sns_contract/lib.rs
+++ astar_sns_contract/lib.rs
@@ -1,74 +1,56 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod follow;
+mod message;
+mod metadata;
+mod post;
+mod profile;
+
 use ink_lang as ink;
 
 #[ink::contract]
 mod astar_sns_contract {
+use ink_env::debug_println;
+use ink_lang::codegen::Env;
+use ink_prelude::vec::Vec;
+use openbrush::storage::Mapping;
+use openbrush::test_utils::accounts;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    pub use crate::follow::*;
+    pub use crate::message::*;
+    pub use crate::metadata::*;
+    pub use crate::post::*;
+
     #[ink(storage)]
     pub struct AstarSnsContract {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        pub profile_map: Mapping<AccountId, Profile>,
+        pub post_map: Mapping<u128, Post>,
+        pub post_map_counter: u128,
+        pub message_list_map: Mapping<u128, Vec<Message>>,
+        pub message_list_map_counter: u128,
     }
 
     impl AstarSnsContract {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {
+                profile_map: Mapping::default(),
+                post_map: Mapping::default(),
+                post_map_counter: 0,
+                message_list_map: Mapping::default(),
+                message_list_map_counter: 0,
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Self::new()
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn debug(&self) {
+            debug_println!("Hello World!");
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let astar_sns_contract = AstarSnsContract::default();
-            assert_eq!(astar_sns_contract.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut astar_sns_contract = AstarSnsContract::new(false);
-            assert_eq!(astar_sns_contract.get(), false);
-            astar_sns_contract.flip();
-            assert_eq!(astar_sns_contract.get(), true);
-        }
-    }
 }
